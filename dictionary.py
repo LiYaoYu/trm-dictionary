b04cfcb3f9aae9361a2f7802daf5b91ac441f959
yaoyu <forever3580ya@gmail.com>
2018-05-27 17:11:52 +0800

    Enable query google xslation by highlighting words

--- dictionary.py
+++ dictionary.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
-import select_area
+import select_area, query
 
+INTERVAL = 0.2
 
 def main():
-    m = select_area.Selection()
-    while True:
-        print(m.get())
+    hl = select_area.Selection(INTERVAL)
+    qr = query.Query()
 
+    while True:
+        word = hl.get_str()
+        xlate = qr.qr_gxlate(word)
+        print(xlate)
+        print(qr.get_gelapsed())
 
 if __name__ == "__main__":
     main()
